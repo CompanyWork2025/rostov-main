e9d5c81b8be57dee36a3145b9f93c376ad4dfe9a
Deepak Kumar <officewebdeveloper2025@gmail.com>
2025-01-23 15:17:00 +0530

    indexing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ViteSitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteSitemap({
+      // Define the hostname (your site’s URL)
+      hostname: 'https://www.rostgmu.in/', // replace with your actual website URL
+    }),
+  ],
   base: '/', // Ensure the base path is set for a root deployment
 });
